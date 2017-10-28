ea4d36b9ff5398e332d9ed737ab4fa9d74a552cb
mzm <43729618@qq.com>
2017-10-28 11:14:46 +0800

    first commit

---
+++ h.php
@@ -0,0 +1,58 @@
+<?php
+    function lj(){
+		$con = mysql_connect("localhost","mzm","Mzm20050527!");
+        if (!$con)
+        {
+            die('Á¬½ÓÊ§°Ü: ' . mysql_error());
+        }
+        mysql_select_db("my_db", $con);
+	}
+	function js(){
+		$con = mysql_connect("localhost","mzm","Mzm20050527!");
+		mysql_close($con);
+	}
+    function z($table,$xm,$value){
+		$z="INSERT INTO '$table' ($xm)VALUES($value)";
+		if (!mysql_query($z))
+        {
+            die('Error: ' . mysql_error());
+        }
+	}
+	function s($table,$value,$xm){
+		$x="gundam."."$table";
+		$z="DELETE FROM gundam.user WHERE  '$xm'='$value'";
+		if (!mysql_query($z))
+        {
+            die('Error: ' . mysql_error());
+        }
+	}
+	function c($p,$q){
+		$x=0;
+		$cha=mysql_query("SELECT * FROM mzm.user where name= '{$p}'");
+        while($row = mysql_fetch_array($cha))
+        {
+            if($row["password"]==$q){
+                $x=3;
+            }else{$x=1;}
+		
+        }	
+		if($x==3){
+				return 2;
+			}else if($x==1){
+				return 1;
+		}
+		else if($x==0){
+				return 0;
+		}
+	}
+	function g($table,$value1,$xm,$value2){
+		$x="gundam."."$table";
+		$z="UPDATE '$x' SET '$xm' = '$value2' WHERE '$xm' = '$value2' ";
+		if (!mysql_query($z))
+        {
+            die('Error: ' . mysql_error());
+        }
+	}
+
+
+?>
---
+++ login.php
@@ -0,0 +1,25 @@
+﻿<?php
+    include 'h.php';
+	lj();
+	$p=$_POST['name'];
+	$q=$_POST['password'];
+	$value=array("$p", "$q");
+	$xm=array("name","password");
+	$a=c($p,$q);
+    if($a==2){
+		echo '恭喜您登陆成功，敢达中文情报站欢迎您！';
+	}
+	else if($a==1){
+		echo '登录失败，您可以尝试：';
+		echo "<a href='add.html'>注册</a>";
+		echo "<a href='login.html'>登录</a>";
+	}
+	else if($a==0){
+		echo 'a';
+		echo "<a href='add.html'>注册</a>";
+		echo "<a href='login.html'>登录</a>";
+	}
+    js();
+		
+	
+?>
